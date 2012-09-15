b586950d6c6178f5dccd488cf20dc2c9d80d85bd
Julian Schutsch <asgardlogis@gmx.de>
2012-09-15 16:08:14 -0400

    u

--- tests/b__rfstest.adb
+++ tests/b__rfstest.adb
@@ -8,30 +8,28 @@ package body ada_main is
 
    E011 : Short_Integer; pragma Import (Ada, E011, "system__soft_links_E");
    E021 : Short_Integer; pragma Import (Ada, E021, "system__exception_table_E");
-   E070 : Short_Integer; pragma Import (Ada, E070, "ada__io_exceptions_E");
-   E099 : Short_Integer; pragma Import (Ada, E099, "ada__strings_E");
-   E105 : Short_Integer; pragma Import (Ada, E105, "ada__strings__maps_E");
-   E046 : Short_Integer; pragma Import (Ada, E046, "ada__tags_E");
-   E056 : Short_Integer; pragma Import (Ada, E056, "ada__streams_E");
-   E072 : Short_Integer; pragma Import (Ada, E072, "interfaces__c_E");
-   E074 : Short_Integer; pragma Import (Ada, E074, "interfaces__c__strings_E");
-   E027 : Short_Integer; pragma Import (Ada, E027, "system__exceptions_E");
-   E069 : Short_Integer; pragma Import (Ada, E069, "system__finalization_root_E");
-   E067 : Short_Integer; pragma Import (Ada, E067, "ada__finalization_E");
-   E090 : Short_Integer; pragma Import (Ada, E090, "system__storage_pools_E");
-   E082 : Short_Integer; pragma Import (Ada, E082, "system__finalization_masters_E");
-   E096 : Short_Integer; pragma Import (Ada, E096, "system__storage_pools__subpools_E");
-   E092 : Short_Integer; pragma Import (Ada, E092, "system__pool_global_E");
-   E080 : Short_Integer; pragma Import (Ada, E080, "system__file_control_block_E");
-   E065 : Short_Integer; pragma Import (Ada, E065, "system__file_io_E");
+   E078 : Short_Integer; pragma Import (Ada, E078, "ada__io_exceptions_E");
+   E095 : Short_Integer; pragma Import (Ada, E095, "ada__strings_E");
+   E101 : Short_Integer; pragma Import (Ada, E101, "ada__strings__maps_E");
+   E045 : Short_Integer; pragma Import (Ada, E045, "ada__tags_E");
+   E058 : Short_Integer; pragma Import (Ada, E058, "ada__streams_E");
+   E080 : Short_Integer; pragma Import (Ada, E080, "interfaces__c_E");
+   E082 : Short_Integer; pragma Import (Ada, E082, "interfaces__c__strings_E");
    E015 : Short_Integer; pragma Import (Ada, E015, "system__secondary_stack_E");
-   E101 : Short_Integer; pragma Import (Ada, E101, "ada__strings__unbounded_E");
-   E077 : Short_Integer; pragma Import (Ada, E077, "system__os_lib_E");
-   E060 : Short_Integer; pragma Import (Ada, E060, "ada__text_io_E");
-   E098 : Short_Integer; pragma Import (Ada, E098, "basics_E");
-   E058 : Short_Integer; pragma Import (Ada, E058, "refcount_E");
-   E055 : Short_Integer; pragma Import (Ada, E055, "resourcefilesystem_E");
-   E119 : Short_Integer; pragma Import (Ada, E119, "streams_E");
+   E060 : Short_Integer; pragma Import (Ada, E060, "system__finalization_root_E");
+   E057 : Short_Integer; pragma Import (Ada, E057, "ada__finalization_E");
+   E097 : Short_Integer; pragma Import (Ada, E097, "ada__strings__unbounded_E");
+   E068 : Short_Integer; pragma Import (Ada, E068, "system__storage_pools_E");
+   E055 : Short_Integer; pragma Import (Ada, E055, "ada__finalization__heap_management_E");
+   E085 : Short_Integer; pragma Import (Ada, E085, "system__os_lib_E");
+   E090 : Short_Integer; pragma Import (Ada, E090, "system__pool_global_E");
+   E088 : Short_Integer; pragma Import (Ada, E088, "system__file_control_block_E");
+   E077 : Short_Integer; pragma Import (Ada, E077, "system__file_io_E");
+   E072 : Short_Integer; pragma Import (Ada, E072, "ada__text_io_E");
+   E094 : Short_Integer; pragma Import (Ada, E094, "basics_E");
+   E070 : Short_Integer; pragma Import (Ada, E070, "refcount_E");
+   E053 : Short_Integer; pragma Import (Ada, E053, "resourcefilesystem_E");
+   E114 : Short_Integer; pragma Import (Ada, E114, "streams_E");
 
    Local_Priority_Specific_Dispatching : constant String := "";
    Local_Interrupt_States : constant String := "";
@@ -39,83 +37,95 @@ package body ada_main is
    Is_Elaborated : Boolean := False;
 
    procedure finalize_library is
+      LE_Set : Boolean;
+      pragma Import (Ada, LE_Set, "__gnat_library_exception_set");
    begin
-      E055 := E055 - 1;
+      E053 := E053 - 1;
       declare
          procedure F1;
          pragma Import (Ada, F1, "resourcefilesystem__finalize_spec");
       begin
          F1;
       end;
-      E058 := E058 - 1;
+      E070 := E070 - 1;
       declare
          procedure F2;
          pragma Import (Ada, F2, "refcount__finalize_spec");
       begin
          F2;
       end;
-      E098 := E098 - 1;
+      E094 := E094 - 1;
       declare
          procedure F3;
          pragma Import (Ada, F3, "basics__finalize_spec");
       begin
          F3;
       end;
-      E060 := E060 - 1;
       declare
          procedure F4;
-         pragma Import (Ada, F4, "ada__text_io__finalize_spec");
+         pragma Import (Ada, F4, "ada__text_io__finalize_body");
       begin
+         E072 := E072 - 1;
          F4;
       end;
       declare
          procedure F5;
-         pragma Import (Ada, F5, "system__file_io__finalize_body");
+         pragma Import (Ada, F5, "ada__text_io__finalize_spec");
       begin
-         E065 := E065 - 1;
          F5;
       end;
-      E101 := E101 - 1;
       declare
          procedure F6;
-         pragma Import (Ada, F6, "ada__strings__unbounded__finalize_spec");
+         pragma Import (Ada, F6, "system__file_io__finalize_body");
       begin
+         E077 := E077 - 1;
          F6;
       end;
-      E082 := E082 - 1;
-      E096 := E096 - 1;
       declare
          procedure F7;
          pragma Import (Ada, F7, "system__file_control_block__finalize_spec");
       begin
-         E080 := E080 - 1;
+         E088 := E088 - 1;
          F7;
       end;
-      E092 := E092 - 1;
+      E090 := E090 - 1;
       declare
          procedure F8;
          pragma Import (Ada, F8, "system__pool_global__finalize_spec");
       begin
          F8;
       end;
+      E055 := E055 - 1;
       declare
          procedure F9;
-         pragma Import (Ada, F9, "system__storage_pools__subpools__finalize_spec");
+         pragma Import (Ada, F9, "ada__finalization__heap_management__finalize_spec");
       begin
          F9;
       end;
+      E097 := E097 - 1;
       declare
          procedure F10;
-         pragma Import (Ada, F10, "system__finalization_masters__finalize_spec");
+         pragma Import (Ada, F10, "ada__strings__unbounded__finalize_spec");
       begin
          F10;
       end;
+      E060 := E060 - 1;
       declare
-         procedure Reraise_Library_Exception_If_Any;
-            pragma Import (Ada, Reraise_Library_Exception_If_Any, "__gnat_reraise_library_exception_if_any");
+         procedure F11;
+         pragma Import (Ada, F11, "system__finalization_root__finalize_spec");
       begin
-         Reraise_Library_Exception_If_Any;
+         F11;
       end;
+      if LE_Set then
+         declare
+            LE : Ada.Exceptions.Exception_Occurrence;
+            pragma Import (Ada, LE, "__gnat_library_exception");
+            procedure Raise_From_Controlled_Operation (X : Ada.Exceptions.Exception_Occurrence;  From_Abort : Boolean);
+            pragma Import (Ada, Raise_From_Controlled_Operation, "__gnat_raise_from_controlled_operation");
+         begin
+            Raise_From_Controlled_Operation (LE, False);
+         end;
+      end if;
    end finalize_library;
 
    procedure adafinal is
@@ -206,58 +216,53 @@ package body ada_main is
       System.Exception_Table'Elab_Body;
       E021 := E021 + 1;
       Ada.Io_Exceptions'Elab_Spec;
-      E070 := E070 + 1;
+      E078 := E078 + 1;
       Ada.Strings'Elab_Spec;
-      E099 := E099 + 1;
+      E095 := E095 + 1;
       Ada.Strings.Maps'Elab_Spec;
       Ada.Tags'Elab_Spec;
       Ada.Streams'Elab_Spec;
-      E056 := E056 + 1;
+      E058 := E058 + 1;
       Interfaces.C'Elab_Spec;
       Interfaces.C.Strings'Elab_Spec;
-      System.Exceptions'Elab_Spec;
-      E027 := E027 + 1;
-      System.Finalization_Root'Elab_Spec;
-      E069 := E069 + 1;
-      Ada.Finalization'Elab_Spec;
-      E067 := E067 + 1;
-      System.Storage_Pools'Elab_Spec;
-      E090 := E090 + 1;
-      System.Finalization_Masters'Elab_Spec;
-      System.Storage_Pools.Subpools'Elab_Spec;
-      System.Pool_Global'Elab_Spec;
-      E092 := E092 + 1;
-      System.File_Control_Block'Elab_Spec;
-      E080 := E080 + 1;
-      E096 := E096 + 1;
-      System.Finalization_Masters'Elab_Body;
       E082 := E082 + 1;
-      E074 := E074 + 1;
-      E072 := E072 + 1;
+      E080 := E080 + 1;
       Ada.Tags'Elab_Body;
-      E046 := E046 + 1;
-      E105 := E105 + 1;
+      E045 := E045 + 1;
+      E101 := E101 + 1;
       System.Soft_Links'Elab_Body;
       E011 := E011 + 1;
       System.Secondary_Stack'Elab_Body;
       E015 := E015 + 1;
+      System.Finalization_Root'Elab_Spec;
+      E060 := E060 + 1;
+      Ada.Finalization'Elab_Spec;
+      E057 := E057 + 1;
       Ada.Strings.Unbounded'Elab_Spec;
-      E101 := E101 + 1;
+      E097 := E097 + 1;
+      System.Storage_Pools'Elab_Spec;
+      E068 := E068 + 1;
+      Ada.Finalization.Heap_Management'Elab_Spec;
+      E055 := E055 + 1;
       System.Os_Lib'Elab_Body;
-      E077 := E077 + 1;
+      E085 := E085 + 1;
+      System.Pool_Global'Elab_Spec;
+      E090 := E090 + 1;
+      System.File_Control_Block'Elab_Spec;
+      E088 := E088 + 1;
       System.File_Io'Elab_Body;
-      E065 := E065 + 1;
+      E077 := E077 + 1;
       Ada.Text_Io'Elab_Spec;
       Ada.Text_Io'Elab_Body;
-      E060 := E060 + 1;
+      E072 := E072 + 1;
       Basics'Elab_Spec;
-      E098 := E098 + 1;
+      E094 := E094 + 1;
       Refcount'Elab_Spec;
-      E058 := E058 + 1;
+      E070 := E070 + 1;
       Resourcefilesystem'Elab_Spec;
-      E055 := E055 + 1;
+      E053 := E053 + 1;
       Streams'Elab_Spec;
-      E119 := E119 + 1;
+      E114 := E114 + 1;
    end adainit;
 
    procedure Ada_Main_Program;
@@ -293,18 +298,17 @@ package body ada_main is
    end;
 
 --  BEGIN Object file/option list
-   --   C:\Users\alexandrus\tw\gpr\basics.o
-   --   C:\Users\alexandrus\tw\gpr\refcount.o
-   --   C:\Users\alexandrus\tw\gpr\resourcefilesystem.o
-   --   C:\Users\alexandrus\tw\gpr\types.o
-   --   C:\Users\alexandrus\tw\gpr\streams.o
-   --   C:\Users\alexandrus\tw\tests\rfstest.o
-   --   -LC:\Users\alexandrus\tw\tests\
-   --   -LC:\Users\alexandrus\tw\gpr\
-   --   -LC:/gnat/2012/lib/gcc/i686-pc-mingw32/4.5.4/adalib/
+   --   /home/alexander/tw/gpr/basics.o
+   --   /home/alexander/tw/gpr/refcount.o
+   --   /home/alexander/tw/gpr/resourcefilesystem.o
+   --   /home/alexander/tw/gpr/types.o
+   --   /home/alexander/tw/gpr/streams.o
+   --   /home/alexander/tw/tests/rfstest.o
+   --   -L/home/alexander/tw/tests/
+   --   -L/home/alexander/tw/gpr/
+   --   -L/usr/gnat/lib/gcc/x86_64-pc-linux-gnu/4.5.3/adalib/
    --   -static
    --   -lgnat
-   --   -Wl,--stack=0x2000000
 --  END Object file/option list   
 
 end ada_main;
